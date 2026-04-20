a531af68c8e8e5311df31df2a2aabb460f469bea
Thangabali <sanketdeotarsetp@gmail.com>
2026-04-20 12:22:36 +0530

    01-folder-creation

---
+++ create-folder/src/App.jsx
@@ -0,0 +1,17 @@
+const App = () => {
+  return (
+    <>
+      <div id="DAD">
+        working On Reactjs
+        <h1>Child Coming from dad</h1>
+      </div>
+
+      <div id="CHACHA">
+        <h1>Coming From CHACHA</h1>
+      </div>
+    </>
+
+  )
+}
+
+export default App

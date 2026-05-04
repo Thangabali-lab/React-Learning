32bd30baeaff14e068c90b6aaf0be923a749cecf
Thangabali <sanketdeotarsetp@gmail.com>
2026-05-04 21:44:29 +0530

    all modules

---
+++ 05-css/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from './component/button/Button'
+import Header from './component/header/header'
+
+const App = () => {
+  return (
+    <div className='parent'>
+      <Button />
+      <Header />
+    </div>
+  )
+}
+
+export default App

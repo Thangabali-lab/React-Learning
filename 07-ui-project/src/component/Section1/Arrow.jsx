85f7daccba68d0065a5405ad7dade20306b64cb4
Thangabali <sanketdeotarsetp@gmail.com>
2026-05-05 13:23:41 +0530

    ui project completed

---
+++ 07-ui-project/src/component/Section1/Arrow.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import 'remixicon/fonts/remixicon.css'
+
+export const Arrow = () => {
+    return (
+        <div className='text-9xl'>
+            <i className="ri-arrow-right-up-line"></i>
+        </div>
+    )
+}
